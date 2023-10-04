f461b046b8b74920bc65a9440eba31ab98d825d2
Your Name <you@example.com>
2023-10-04 12:40:01 +0200

    UPDATE

--- submit.php
+++ submit.php
@@ -6,8 +6,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $selectedOptionsObject = $data["selectedOptions"];
         $phone = $selectedOptionsObject["Номер телефона"];
 
-        $telegramBotToken = "YOUR_TELEGRAM_BOT_TOKEN";
-        $chatId = "YOUR_CHAT_ID";
+        $telegramBotToken = "6540913409:AAHROLebCk82OL08su9kDPcsoc1KLrxTKOk";
+        $chatId = "-937143529";
 
         $telegramMessage = "Номер телефона: $phone\nВыбранные варианты: " . json_encode($selectedOptionsObject);
         $telegramApiUrl = "https://api.telegram.org/bot$telegramBotToken/sendMessage?chat_id=$chatId&text=" . urlencode($telegramMessage);
@@ -15,9 +15,9 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
         // Дополнительная логика для отправки данных в AmoCrm
         // Шаг 1: Авторизация в AmoCRM
-        $subdomain = 'your_subdomain'; // Замените на ваш поддомен AmoCRM
-        $user_login = 'your_login'; // Ваш логин в AmoCRM
-        $user_hash = 'your_api_key'; // Ваш API-ключ
+        $subdomain = 'palmieri'; // Замените на ваш поддомен AmoCRM
+        $user_login = 'oleg.palmieri@ya.ru'; // Ваш логин в AmoCRM
+        $user_hash = 'sapFsOvsKovA9WK73uSJufb5fCbQAe1huiXbNqOsq5C0VA1tf6yTiCrqonPa9Gho'; // Ваш API-ключ
 
         $auth_data = [
             'USER_LOGIN' => $user_login,
